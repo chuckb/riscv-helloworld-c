494c9ecce7281625964d2beaad05f20026b53dd5
Chuck Benedict <chuck_benedict@yahoo.com>
2024-07-20 06:47:13 -0700

    Tab clean

--- hello.c
+++ hello.c
@@ -23,7 +23,7 @@ void uart_puts(const char *str) {
 }
 
 void main() {
-	UART0_FCR = UARTFCR_FFENA;              // Set the FIFO for polled operation
+  UART0_FCR = UARTFCR_FFENA;              // Set the FIFO for polled operation
   uart_puts("Hello World!\n");            // Write the string to the UART
   while (1);                              // Loop forever to prevent program from ending
 }
